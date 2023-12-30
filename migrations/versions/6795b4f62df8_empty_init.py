52cba7a006f499738c4e817489eefb3d54ab60db
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-30 11:49:45 +0300

    Done

---
+++ migrations/versions/6795b4f62df8_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 6795b4f62df8
+Revises: 
+Create Date: 2023-12-30 11:45:53.315750
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6795b4f62df8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
